2837a40554fd61ddd013096cc5af68db40e8abc6
Urioxi <urioxa@gmail.com>
2025-12-25 17:16:56 +0100

    patch général 3

---
+++ static/test.js
@@ -0,0 +1,2 @@
+// Test JavaScript - Version simplifiée
+document.body.insertAdjacentHTML('beforeend', '<div style="position: fixed; top: 100px; left: 20px; background: green; color: white; padding: 10px; border-radius: 5px; z-index: 10000;">JS EXECUTE</div>');
